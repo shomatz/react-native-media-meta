15ab16997a14ed27b4e3b5855f467641f0d8b183
shomatz <34609238+shomatz@users.noreply.github.com>
2024-02-17 03:06:44 +0700

    Update RNMediaMeta.m

--- ios/RNMediaMeta/RNMediaMeta.m
+++ ios/RNMediaMeta/RNMediaMeta.m
@@ -38,85 +38,90 @@ RCT_EXPORT_METHOD(get:(NSString *)path
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-  NSFileManager *fileManager = [NSFileManager defaultManager];
-
-  BOOL isDir;
-  if (![fileManager fileExistsAtPath:path isDirectory:&isDir] || isDir){
-    NSError *err = [NSError errorWithDomain:@"file not found" code:-15 userInfo:nil];
-    reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
-    return;
-  }
-
-  NSMutableDictionary *result = [NSMutableDictionary new];
-  NSDictionary *assetOptions = @{AVURLAssetPreferPreciseDurationAndTimingKey: @YES};
-  AVURLAsset *asset = [AVURLAsset URLAssetWithURL:[NSURL fileURLWithPath:path] options:assetOptions];
-
-  NSArray *keys = [NSArray arrayWithObjects:@"commonMetadata", nil];
-  [asset loadValuesAsynchronouslyForKeys:keys completionHandler:^{
-    // string keys
-    for (NSString *key in [self metadatas]) {
-      NSArray *items = [AVMetadataItem metadataItemsFromArray:asset.commonMetadata
-                                                     withKey:key
-                                                    keySpace:AVMetadataKeySpaceCommon];
-      for (AVMetadataItem *item in items) {
-        [result setObject:item.value forKey:key];
-      }
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    BOOL isDir;
+    if (![fileManager fileExistsAtPath:path isDirectory:&isDir]){
+        NSError *err = [NSError errorWithDomain:@"file not found" code:-15 userInfo:nil];
+        reject([NSString stringWithFormat: @"%lu", (long)err.code], err.localizedDescription, err);
+        return;
     }
-
-    if (options[@"getThumb"]) {
-      UIImage *thumbnail;
-      NSArray *artworks = [AVMetadataItem metadataItemsFromArray:asset.commonMetadata
-                                                        withKey:AVMetadataCommonKeyArtwork
-                                                        keySpace:AVMetadataKeySpaceCommon];
-      // artwork thumb
-      for (AVMetadataItem *item in artworks) {
-        thumbnail = [UIImage imageWithData:item.value];
-      }
-      if (thumbnail) {
-        [result setObject:@(thumbnail.size.width) forKey:@"width"];
-        [result setObject:@(thumbnail.size.height) forKey:@"height"];
-        NSString *filePath = [NSTemporaryDirectory() stringByAppendingString:[[NSUUID UUID] UUIDString]];
-        filePath = [filePath stringByAppendingString:@".png"];
-        [UIImagePNGRepresentation(thumbnail) writeToFile:filePath atomically:true];
-        [result setObject:filePath forKey:@"thumb"];
-      }
-
-      // video frame thumb
-      AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc]initWithAsset:asset];
-      imageGenerator.appliesPreferredTrackTransform = YES;
-      CMTime duration = [asset duration];
-      CMTime time = CMTimeMake(duration.value / 2, duration.timescale);
-
-      CGImageRef imageRef = [imageGenerator copyCGImageAtTime:time actualTime:NULL error:NULL];
-      thumbnail = [UIImage imageWithCGImage:imageRef];
-      if (thumbnail) {
-        [result setObject:@(thumbnail.size.width) forKey:@"width"];
-        [result setObject:@(thumbnail.size.height) forKey:@"height"];
-        [result setObject:@((CMTimeGetSeconds(asset.duration) * 1000)) forKey:@"duration"];
-        NSString *filePath = [NSTemporaryDirectory() stringByAppendingString:[[NSUUID UUID] UUIDString]];
-        filePath = [filePath stringByAppendingString:@".png"];
-        [UIImagePNGRepresentation(thumbnail) writeToFile:filePath atomically:true];
-        [result setObject:filePath forKey:@"thumb"];
-      }
-      CGImageRelease(imageRef);
-    }
-
+    
     NSError *error = nil;
     NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:path error:&error];
-
+    
     if (error) {
-      NSString *codeWithDomain = [NSString stringWithFormat:@"E%@%zd", error.domain.uppercaseString, error.code];
-      reject(codeWithDomain, error.localizedDescription, error);
+        NSString *codeWithDomain = [NSString stringWithFormat:@"E%@%zd", error.domain.uppercaseString, error.code];
+        reject(codeWithDomain, error.localizedDescription, error);
     }
-      
+    
+    NSMutableDictionary *result = [NSMutableDictionary new];
+    
     [result setObject:@([(NSDate *)[attributes objectForKey:NSFileCreationDate] timeIntervalSince1970]) forKey:@"ctime"];
     [result setObject:@([(NSDate *)[attributes objectForKey:NSFileModificationDate] timeIntervalSince1970]) forKey:@"mtime"];
     [result setObject:[attributes objectForKey:NSFileSize] forKey:@"size"];
-    [result setObject:[attributes objectForKey:NSFileType] forKey:@"type"];
+    [result setObject:isDir ? @"directory" : @"file" forKey:@"type"];
     [result setObject:@([[NSString stringWithFormat:@"%ld", (long)[(NSNumber *)[attributes objectForKey:NSFilePosixPermissions] integerValue]] integerValue]) forKey:@"mode"];
-
-    resolve(result);
-  }];
+    
+    if (isDir) {
+        resolve(result);
+    } else {
+        NSDictionary *assetOptions = @{AVURLAssetPreferPreciseDurationAndTimingKey: @YES};
+        AVURLAsset *asset = [AVURLAsset URLAssetWithURL:[NSURL fileURLWithPath:path] options:assetOptions];
+        
+        NSArray *keys = [NSArray arrayWithObjects:@"commonMetadata", nil];
+        [asset loadValuesAsynchronouslyForKeys:keys completionHandler:^{
+            // string keys
+            for (NSString *key in [self metadatas]) {
+                NSArray *items = [AVMetadataItem metadataItemsFromArray:asset.commonMetadata
+                                                                withKey:key
+                                                               keySpace:AVMetadataKeySpaceCommon];
+                for (AVMetadataItem *item in items) {
+                    [result setObject:item.value forKey:key];
+                }
+            }
+            
+            if (options[@"getThumb"]) {
+                UIImage *thumbnail;
+                NSArray *artworks = [AVMetadataItem metadataItemsFromArray:asset.commonMetadata
+                                                                   withKey:AVMetadataCommonKeyArtwork
+                                                                  keySpace:AVMetadataKeySpaceCommon];
+                // artwork thumb
+                for (AVMetadataItem *item in artworks) {
+                    thumbnail = [UIImage imageWithData:item.value];
+                }
+                if (thumbnail) {
+                    [result setObject:@(thumbnail.size.width) forKey:@"width"];
+                    [result setObject:@(thumbnail.size.height) forKey:@"height"];
+                    NSString *filePath = [NSTemporaryDirectory() stringByAppendingString:[[NSUUID UUID] UUIDString]];
+                    filePath = [filePath stringByAppendingString:@".png"];
+                    [UIImagePNGRepresentation(thumbnail) writeToFile:filePath atomically:true];
+                    [result setObject:filePath forKey:@"thumb"];
+                }
+                
+                // video frame thumb
+                AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc]initWithAsset:asset];
+                imageGenerator.appliesPreferredTrackTransform = YES;
+                CMTime duration = [asset duration];
+                CMTime time = CMTimeMake(duration.value / 2, duration.timescale);
+                
+                CGImageRef imageRef = [imageGenerator copyCGImageAtTime:time actualTime:NULL error:NULL];
+                thumbnail = [UIImage imageWithCGImage:imageRef];
+                if (thumbnail) {
+                    [result setObject:@(thumbnail.size.width) forKey:@"width"];
+                    [result setObject:@(thumbnail.size.height) forKey:@"height"];
+                    [result setObject:@((CMTimeGetSeconds(asset.duration) * 1000)) forKey:@"duration"];
+                    NSString *filePath = [NSTemporaryDirectory() stringByAppendingString:[[NSUUID UUID] UUIDString]];
+                    filePath = [filePath stringByAppendingString:@".png"];
+                    [UIImagePNGRepresentation(thumbnail) writeToFile:filePath atomically:true];
+                    [result setObject:filePath forKey:@"thumb"];
+                }
+                CGImageRelease(imageRef);
+            }
+            
+            resolve(result);
+        }];
+    }
 }
 
 @end
